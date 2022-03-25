24260cb102f24560dd21f223f2676b85cf23308b
safderun <burakberkkeskin@gmail.com>
2022-03-25 21:29:56 +0300

    fix

--- genes.py
+++ genes.py
@@ -9,7 +9,7 @@ class Kromozom():
 
 def createFirstPopulation():
   population: Kromozom = []
-  for i in range(0,3):
+  for i in range(0,32):
     firstParent = [0,1,2,3,4,5,6,7]
     secondParent = [0,1,2,3,4,5,6,7]
     gens = []
@@ -29,17 +29,6 @@ def createFirstPopulation():
 def hvIntersection(gene):
   rank = 0
   for i in range(0, len(gene)):
-    crossGenes:list = []
-
-    for j in range(1, len(gene)+1):
-      crossGenes.append([gene[i][0]+j, gene[i][1]+j])
-      crossGenes.append([gene[i][0]+j, gene[i][1]-j])
-      crossGenes.append([gene[i][0]-j, gene[i][1]+j])
-      crossGenes.append([gene[i][0]-j, gene[i][1]-j])
-    for crossGene in crossGenes:
-      if crossGene in gene:
-        rank += 1
-
     for j in range(i+1, len(gene)):
       if gene[i][0]==gene[j][0]:
         rank += 1
--- main.py
+++ main.py
@@ -9,20 +9,20 @@ population: genes.Kromozom = []
 
 def main():
   population = genes.createFirstPopulation()
-  #population.append(genes.Kromozom([[0,0],[0,1],[2,6],[3,3],[4,4],[5,5],[6,6],[7,5]], 0))
-  #population.append(genes.Kromozom([[0,2],[1,4],[2,2],[3,0],[4,3],[5,1],[6,7],[7,7]], 0))
+  # population.append(genes.Kromozom([[0,0],[0,1],[2,6],[3,3],[4,4],[5,5],[6,6],[7,5]], 0))
+  # population.append(genes.Kromozom([[0,2],[1,4],[2,2],[3,0],[4,3],[5,1],[6,7],[7,7]], 0))
   #population.append(genes.Kromozom([[0,2],[1,4],[2,6],[3,0],[4,3],[5,1],[6,7],[7,5]], 0))
 
   for i in range(0, len(population)):
-    #intersectionCount = genes.crossIntersection(population[i].gene)
-    intersectionCount = genes.hvIntersection(population[i].gene)
+    intersectionCount = genes.crossIntersection(population[i].gene)
+    intersectionCount += genes.hvIntersection(population[i].gene)
     population[i].rank += intersectionCount
 
 
   ## Sort And Check if there is a solution in starting population
   population.sort(key=lambda x: x.rank)
   if population[0].rank == 0:
-    print("Solution found in starting population: ", population[0].gene)
+    print("Solution found: ", population[0].gene)
     exit()
 
   ## Print Listed Population 
@@ -45,13 +45,13 @@ def main():
       count += 1
 
       # checkCrossStartTime = timer()
-      #intersectionCount = genes.crossIntersection(i)
-      intersectionCount = genes.hvIntersection(i)
+      intersectionCount = genes.crossIntersection(i)
+      intersectionCount += genes.hvIntersection(i)
       # checkCrossEndTime = timer()
       # print("Check took: ", timedelta(seconds=checkCrossEndTime-checkCrossStartTime))
       if intersectionCount == 0:
         endAll = timer()
-        print("\nIn", endAll-startAll, "Seconds\n Solution found: ", i)
+        print("\nIn", count, "Steps,", endAll-startAll, "Seconds\n Solution found: ", i)
         exit()
 
 
